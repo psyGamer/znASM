edbb2a1abecc9b80c5df4fcbebf9891d79ffd72d
psyGamer <psygamer21@gmail.com>
2024-11-02 14:03:56 +0100

    Move function analyzer into own file

--- compiler/Sema.zig
+++ compiler/Sema.zig
@@ -403,201 +403,7 @@ fn gatherFunctionSymbol(sema: *Sema, sym_loc: SymbolLocation, module_idx: u32, n
 }
 
 fn analyzeFunction(sema: *Sema, sym_loc: SymbolLocation, module_idx: ModuleIndex, func: *Symbol.Function) !void {
-    const Analyzer = struct {
-        const Analyzer = @This();
-
-        ast: *const Ast,
-        sema: *Sema,
-
-        func: *Symbol.Function,
-        sym_loc: SymbolLocation,
-
-        ir: std.ArrayListUnmanaged(Ir) = .empty,
-
-        // State
-
-        /// Size of the A-Register / Memory instructions
-        mem_size: Instruction.SizeMode = .none,
-        /// Size of the X/Y-Registers
-        idx_size: Instruction.SizeMode = .none,
-
-        pub fn handleFnDef(anal: *Analyzer, node_idx: NodeIndex) !void {
-            try anal.handleBlock(anal.ast.node_data[node_idx].fn_def.block);
-        }
-
-        pub fn handleBlock(anal: *Analyzer, node_idx: NodeIndex) !void {
-            const range = anal.ast.node_data[node_idx].sub_range;
-            for (range.start..range.end) |extra_idx| {
-                const child_idx = anal.ast.extra_data[extra_idx];
-
-                switch (anal.ast.node_tags[child_idx]) {
-                    .instruction => try anal.handleInstruction(child_idx),
-                    .label => try anal.handleLabel(child_idx),
-                    else => unreachable,
-                }
-            }
-        }
-
-        pub fn handleInstruction(anal: *Analyzer, node_idx: NodeIndex) !void {
-            const opcode_name = anal.ast.parseIdentifier(anal.ast.node_tokens[node_idx]);
-
-            const opcode = get_opcode: {
-                inline for (std.meta.fields(Instruction)) |field| {
-                    @setEvalBranchQuota(std.meta.fields(InstructionType).len * 1000);
-                    const curr_opcode = comptime std.meta.stringToEnum(InstructionType, field.name).?;
-                    const mnemonic = field.name[0..3];
-
-                    if (std.mem.eql(u8, mnemonic, opcode_name)) {
-                        if (field.type == void and anal.ast.isToken(anal.ast.node_tokens[node_idx] + 1, .new_line)) {
-                            break :get_opcode curr_opcode;
-                        }
-                        if (field.type == Instruction.Imm816 and anal.ast.areTokens(anal.ast.node_tokens[node_idx] + 1, &.{ .int_literal, .new_line })) {
-                            break :get_opcode curr_opcode;
-                        }
-
-                        if (curr_opcode == .bra or
-                            curr_opcode == .jmp or curr_opcode == .jml or curr_opcode == .jsr or curr_opcode == .jsl)
-                        {
-                            if (anal.ast.areTokens(anal.ast.node_tokens[node_idx] + 1, &.{ .ident, .new_line })) {
-                                break :get_opcode curr_opcode;
-                            }
-                        }
-                    }
-                }
-
-                try anal.sema.errors.append(anal.sema.allocator, .{
-                    .tag = .invalid_opcode,
-                    .type = .err,
-                    .ast = anal.ast,
-                    .token = anal.ast.node_tokens[node_idx],
-                });
-                return;
-            };
-
-            const instr: Instruction, const reloc: ?Relocation = get_instr: switch (opcode) {
-                inline else => |t| {
-                    const field = comptime find_field: {
-                        for (std.meta.fields(Instruction)) |field| {
-                            if (std.mem.eql(u8, field.name, @tagName(t))) {
-                                break :find_field field;
-                            }
-                        }
-                        unreachable;
-                    };
-
-                    const operand_token = anal.ast.node_tokens[node_idx] + 1;
-
-                    if (field.type == void) {
-                        break :get_instr .{ @unionInit(Instruction, field.name, {}), null };
-                    }
-                    if (field.type == Instruction.Imm816) {
-                        const size_type = opcode.size_type();
-                        const curr_size = switch (size_type) {
-                            .mem => anal.mem_size,
-                            .idx => anal.idx_size,
-                            .none => unreachable,
-                        };
-
-                        if (curr_size == .none) {
-                            try anal.sema.errors.append(anal.sema.allocator, .{
-                                .tag = .undefined_size_mode,
-                                .type = .err,
-                                .ast = anal.ast,
-                                .token = operand_token,
-                                .extra = .{ .size_type = size_type },
-                            });
-                            return;
-                        }
-                        if (curr_size == .@"8bit") {
-                            const value = try anal.sema.parseInt(u8, anal.ast, operand_token);
-                            break :get_instr .{ @unionInit(Instruction, field.name, .{ .imm8 = value }), null };
-                        }
-                        if (curr_size == .@"16bit") {
-                            const value = try anal.sema.parseInt(u16, anal.ast, operand_token);
-                            break :get_instr .{ @unionInit(Instruction, field.name, .{ .imm16 = value }), null };
-                        }
-                    }
-
-                    if (opcode == .bra or
-                        opcode == .jmp or opcode == .jml or opcode == .jsr or opcode == .jsl)
-                    {
-                        break :get_instr .{ @unionInit(Instruction, field.name, undefined), .{
-                            .type = switch (opcode) {
-                                .bra => .rel8,
-                                .jmp, .jsr => .addr16,
-                                .jml, .jsl => .addr24,
-                                else => unreachable,
-                            },
-                            .target_sym = .parse(anal.ast.parseIdentifier(operand_token), anal.sym_loc.module),
-                            .target_offset = 0,
-                        } };
-                    }
-
-                    unreachable;
-                },
-            };
-
-            try anal.ir.append(anal.sema.allocator, .{
-                .tag = .{ .instruction = .{ .instr = instr, .reloc = reloc } },
-                .node = node_idx,
-            });
-        }
-
-        pub fn handleLabel(anal: *Analyzer, node_idx: NodeIndex) !void {
-            const label_token = anal.ast.node_tokens[node_idx];
-            const label = anal.ast.parseIdentifier(label_token);
-
-            if (anal.sema.lookupSymbol(.{ .module = anal.sym_loc.module, .name = label })) |existing_sym| {
-                try anal.sema.errors.append(anal.sema.allocator, .{
-                    .tag = .duplicate_label,
-                    .type = .err,
-                    .ast = anal.ast,
-                    .token = label_token,
-                });
-                try anal.sema.errors.append(anal.sema.allocator, .{
-                    .tag = .existing_sym,
-                    .type = .note,
-                    .ast = anal.ast,
-                    .token = anal.ast.node_tokens[
-                        switch (existing_sym) {
-                            .function => |existing_func| existing_func.node,
-                            else => @panic("TODO"),
-                        }
-                    ],
-                });
-                return error.AnalyzeFailed;
-            }
-            for (anal.ir.items) |ir| {
-                if (ir.tag != .label) {
-                    continue;
-                }
-
-                const existing_label = ir.tag.label;
-                if (std.mem.eql(u8, label, existing_label)) {
-                    try anal.sema.errors.append(anal.sema.allocator, .{
-                        .tag = .duplicate_label,
-                        .type = .err,
-                        .ast = anal.ast,
-                        .token = label_token,
-                    });
-                    try anal.sema.errors.append(anal.sema.allocator, .{
-                        .tag = .existing_label,
-                        .type = .note,
-                        .ast = anal.ast,
-                        .token = anal.ast.node_tokens[ir.node],
-                    });
-                }
-                return error.AnalyzeFailed;
-            }
-
-            try anal.ir.append(anal.sema.allocator, .{
-                .tag = .{ .label = label },
-                .node = node_idx,
-            });
-        }
-    };
-
-    var analyzer: Analyzer = .{
+    var analyzer: @import("sema/FunctionAnalyzer.zig") = .{
         .ast = &sema.modules[module_idx].ast,
         .sema = sema,
         .func = func,
@@ -621,7 +427,8 @@ fn createSymbol(sema: *Sema, sym_loc: SymbolLocation, sym: Symbol) !void {
     try sema.symbols.append(sema.allocator, .{ .sym = sym, .loc = sym_loc });
 }
 
-fn lookupSymbol(sema: Sema, sym_loc: SymbolLocation) ?Symbol {
+/// Searches for the specified symbol
+pub fn lookupSymbol(sema: Sema, sym_loc: SymbolLocation) ?Symbol {
     if (sema.symbol_map.get(sym_loc.module)) |module_symbols| {
         if (module_symbols.get(sym_loc.name)) |sym_idx| {
             return sema.symbols.items(.sym)[sym_idx];
@@ -631,7 +438,7 @@ fn lookupSymbol(sema: Sema, sym_loc: SymbolLocation) ?Symbol {
 }
 
 /// Tries parsing an integer and reports an error on failure
-fn parseInt(sema: *Sema, comptime T: type, ast: *const Ast, token_idx: Ast.TokenIndex) AnalyzeError!T {
+pub fn parseInt(sema: *Sema, comptime T: type, ast: *const Ast, token_idx: Ast.TokenIndex) AnalyzeError!T {
     return ast.parseIntLiteral(T, token_idx) catch |err| {
         try sema.errors.append(sema.allocator, .{
             .tag = switch (err) {
---
+++ compiler/sema/FunctionAnalyzer.zig
@@ -0,0 +1,202 @@
+const std = @import("std");
+const Ast = @import("../Ast.zig");
+const NodeIndex = Ast.NodeIndex;
+const Sema = @import("../Sema.zig");
+const Ir = @import("../ir.zig").Ir;
+const Symbol = @import("../symbol.zig").Symbol;
+const SymbolLocation = @import("../symbol.zig").SymbolLocation;
+const Instruction = @import("../instruction.zig").Instruction;
+const InstructionType = @import("../instruction.zig").InstructionType;
+const Relocation = @import("../CodeGen.zig").Relocation;
+
+const Analyzer = @This();
+
+ast: *const Ast,
+sema: *Sema,
+
+func: *Symbol.Function,
+sym_loc: SymbolLocation,
+
+ir: std.ArrayListUnmanaged(Ir) = .empty,
+
+// State
+
+/// Size of the A-Register / Memory instructions
+mem_size: Instruction.SizeMode = .none,
+/// Size of the X/Y-Registers
+idx_size: Instruction.SizeMode = .none,
+
+pub fn handleFnDef(anal: *Analyzer, node_idx: NodeIndex) !void {
+    try anal.handleBlock(anal.ast.node_data[node_idx].fn_def.block);
+}
+
+pub fn handleBlock(anal: *Analyzer, node_idx: NodeIndex) !void {
+    const range = anal.ast.node_data[node_idx].sub_range;
+    for (range.start..range.end) |extra_idx| {
+        const child_idx = anal.ast.extra_data[extra_idx];
+
+        switch (anal.ast.node_tags[child_idx]) {
+            .instruction => try anal.handleInstruction(child_idx),
+            .label => try anal.handleLabel(child_idx),
+            else => unreachable,
+        }
+    }
+}
+
+pub fn handleInstruction(anal: *Analyzer, node_idx: NodeIndex) !void {
+    const opcode_name = anal.ast.parseIdentifier(anal.ast.node_tokens[node_idx]);
+
+    const opcode = get_opcode: {
+        inline for (std.meta.fields(Instruction)) |field| {
+            @setEvalBranchQuota(std.meta.fields(InstructionType).len * 1000);
+            const curr_opcode = comptime std.meta.stringToEnum(InstructionType, field.name).?;
+            const mnemonic = field.name[0..3];
+
+            if (std.mem.eql(u8, mnemonic, opcode_name)) {
+                if (field.type == void and anal.ast.isToken(anal.ast.node_tokens[node_idx] + 1, .new_line)) {
+                    break :get_opcode curr_opcode;
+                }
+                if (field.type == Instruction.Imm816 and anal.ast.areTokens(anal.ast.node_tokens[node_idx] + 1, &.{ .int_literal, .new_line })) {
+                    break :get_opcode curr_opcode;
+                }
+
+                if (curr_opcode == .bra or
+                    curr_opcode == .jmp or curr_opcode == .jml or curr_opcode == .jsr or curr_opcode == .jsl)
+                {
+                    if (anal.ast.areTokens(anal.ast.node_tokens[node_idx] + 1, &.{ .ident, .new_line })) {
+                        break :get_opcode curr_opcode;
+                    }
+                }
+            }
+        }
+
+        try anal.sema.errors.append(anal.sema.allocator, .{
+            .tag = .invalid_opcode,
+            .type = .err,
+            .ast = anal.ast,
+            .token = anal.ast.node_tokens[node_idx],
+        });
+        return;
+    };
+
+    const instr: Instruction, const reloc: ?Relocation = get_instr: switch (opcode) {
+        inline else => |t| {
+            const field = comptime find_field: {
+                for (std.meta.fields(Instruction)) |field| {
+                    if (std.mem.eql(u8, field.name, @tagName(t))) {
+                        break :find_field field;
+                    }
+                }
+                unreachable;
+            };
+
+            const operand_token = anal.ast.node_tokens[node_idx] + 1;
+
+            if (field.type == void) {
+                break :get_instr .{ @unionInit(Instruction, field.name, {}), null };
+            }
+            if (field.type == Instruction.Imm816) {
+                const size_type = opcode.size_type();
+                const curr_size = switch (size_type) {
+                    .mem => anal.mem_size,
+                    .idx => anal.idx_size,
+                    .none => unreachable,
+                };
+
+                if (curr_size == .none) {
+                    try anal.sema.errors.append(anal.sema.allocator, .{
+                        .tag = .undefined_size_mode,
+                        .type = .err,
+                        .ast = anal.ast,
+                        .token = operand_token,
+                        .extra = .{ .size_type = size_type },
+                    });
+                    return;
+                }
+                if (curr_size == .@"8bit") {
+                    const value = try anal.sema.parseInt(u8, anal.ast, operand_token);
+                    break :get_instr .{ @unionInit(Instruction, field.name, .{ .imm8 = value }), null };
+                }
+                if (curr_size == .@"16bit") {
+                    const value = try anal.sema.parseInt(u16, anal.ast, operand_token);
+                    break :get_instr .{ @unionInit(Instruction, field.name, .{ .imm16 = value }), null };
+                }
+            }
+
+            if (opcode == .bra or
+                opcode == .jmp or opcode == .jml or opcode == .jsr or opcode == .jsl)
+            {
+                break :get_instr .{ @unionInit(Instruction, field.name, undefined), .{
+                    .type = switch (opcode) {
+                        .bra => .rel8,
+                        .jmp, .jsr => .addr16,
+                        .jml, .jsl => .addr24,
+                        else => unreachable,
+                    },
+                    .target_sym = .parse(anal.ast.parseIdentifier(operand_token), anal.sym_loc.module),
+                    .target_offset = 0,
+                } };
+            }
+
+            unreachable;
+        },
+    };
+
+    try anal.ir.append(anal.sema.allocator, .{
+        .tag = .{ .instruction = .{ .instr = instr, .reloc = reloc } },
+        .node = node_idx,
+    });
+}
+
+pub fn handleLabel(anal: *Analyzer, node_idx: NodeIndex) !void {
+    const label_token = anal.ast.node_tokens[node_idx];
+    const label = anal.ast.parseIdentifier(label_token);
+
+    if (anal.sema.lookupSymbol(.{ .module = anal.sym_loc.module, .name = label })) |existing_sym| {
+        try anal.sema.errors.append(anal.sema.allocator, .{
+            .tag = .duplicate_label,
+            .type = .err,
+            .ast = anal.ast,
+            .token = label_token,
+        });
+        try anal.sema.errors.append(anal.sema.allocator, .{
+            .tag = .existing_sym,
+            .type = .note,
+            .ast = anal.ast,
+            .token = anal.ast.node_tokens[
+                switch (existing_sym) {
+                    .function => |existing_func| existing_func.node,
+                    else => @panic("TODO"),
+                }
+            ],
+        });
+        return error.AnalyzeFailed;
+    }
+    for (anal.ir.items) |ir| {
+        if (ir.tag != .label) {
+            continue;
+        }
+
+        const existing_label = ir.tag.label;
+        if (std.mem.eql(u8, label, existing_label)) {
+            try anal.sema.errors.append(anal.sema.allocator, .{
+                .tag = .duplicate_label,
+                .type = .err,
+                .ast = anal.ast,
+                .token = label_token,
+            });
+            try anal.sema.errors.append(anal.sema.allocator, .{
+                .tag = .existing_label,
+                .type = .note,
+                .ast = anal.ast,
+                .token = anal.ast.node_tokens[ir.node],
+            });
+        }
+        return error.AnalyzeFailed;
+    }
+
+    try anal.ir.append(anal.sema.allocator, .{
+        .tag = .{ .label = label },
+        .node = node_idx,
+    });
+}
