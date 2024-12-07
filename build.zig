d6b8a004c06acfacf9eb7e1c0028e6adc4b8560e
psyGamer <psygamer21@gmail.com>
2024-12-07 16:28:01 +0100

    Remove old project files

--- build.zig
+++ build.zig
@@ -4,22 +4,10 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const use_llvm = b.option(bool, "use-llvm", "Use LLVM for compiling SNasm") orelse (optimize != .Debug or target.result.os.tag == .windows);
-
-    // const znasm_mod = b.addModule("znasm", .{
-    //     .root_source_file = b.path("znasm/lib.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // const snes_mod = b.addModule("snes", .{
-    //     .root_source_file = b.path("snes/lib.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // snes_mod.addImport("znasm", znasm_mod);
+    const use_llvm = b.option(bool, "use-llvm", "Use LLVM for compiling znASM") orelse (optimize != .Debug or target.result.os.tag == .windows);
 
     const compiler_exe = b.addExecutable(.{
-        .name = "compiler",
+        .name = "znasm",
         .root_source_file = b.path("compiler/main.zig"),
         .target = target,
         .optimize = optimize,
@@ -47,16 +35,4 @@ pub fn build(b: *std.Build) void {
 
     const test_compiler_step = b.step("test", "Run znASM compiler unit tests");
     test_compiler_step.dependOn(&run_compiler_tests.step);
-
-    // Only required for ZLS
-    // const lib = b.addStaticLibrary(.{
-    //     .name = "znasm",
-    //     .root_source_file = b.path("znasm/lib.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // lib.root_module.addImport("znasm", znasm_mod);
-    // lib.root_module.addImport("snes", snes_mod);
-
-    // b.installArtifact(lib);
 }
